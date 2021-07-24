f763a191f0269c793dcfacbad5b1493b94f9f474
MTHuang <siksj3@gmail.com>
2021-07-25 01:39:36 +0800

    add new file(Apple)

---
+++ HW09-Apple官網購買頁面/Apple_products.js
@@ -0,0 +1,65 @@
+let iphone12pro = {
+    basepicture: [
+        { name:"pro", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-family-hero-all?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;.v=1604021662000" },
+        { name:"promax", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-max-family-hero-all?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;.v=1604021663000" }
+
+    ],
+    type: [
+        { name:"pro", storage:"128GB", price:33900 },
+        { name:"pro", storage:"256GB", price:37400 },
+        { name:"pro", storage:"512GB", price:44400 },
+        { name:"promax", storage:"128GB", price:37900},
+        { name:"promax", storage:"256GB", price:41400},
+        { name:"promax", storage:"512GB", price:48400}
+    ],
+    picture: [
+        { name:"pro",color:"graphite", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-graphite-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021660000"},
+        { name:"pro",color:"silver", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-silver-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021661000"},
+        { name:"pro",color:"gold", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-gold-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021659000"},
+        { name:"pro",color:"blue", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021661000"},
+        { name:"promax",color:"graphite", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-max-graphite-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000"},
+        { name:"promax",color:"silver", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-max-silver-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000"},
+        { name:"promax",color:"gold", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-max-gold-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021660000"},
+        { name:"promax",color:"blue", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-max-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000"}
+    ]
+}
+
+let iPadAir = [
+    { "id":"A001", "color": "gray",  "price": 18900, "storage":"64GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-spacegray-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650646000" },
+    { "id":"A002", "color": "silver",  "price": 18900, "storage":"64GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-silver-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650645000" },
+    { "id":"A003", "color": "rose",  "price": 18900, "storage":"64GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-gold-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"A004", "color": "green",  "price": 18900, "storage":"64GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-green-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"A005", "color": "blue",  "price": 18900, "storage":"64GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-blue-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"B001", "color": "gray",  "price": 23900, "storage":"256GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-spacegray-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650646000" },
+    { "id":"B002", "color": "silver",  "price": 23900, "storage":"256GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-silver-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650645000" },
+    { "id":"B003", "color": "rose",  "price": 23900, "storage":"256GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-gold-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"B004", "color": "green",  "price": 23900, "storage":"256GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-green-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"B005", "color": "blue",  "price": 23900, "storage":"256GB", "network": "Wi-Fi", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-blue-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"C001", "color": "gray",  "price": 23200, "storage":"64GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-spacegray-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650646000" },
+    { "id":"C002", "color": "silver",  "price": 23200, "storage":"64GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-silver-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650645000" },
+    { "id":"C003", "color": "rose",  "price": 23200, "storage":"64GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-gold-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"C004", "color": "green",  "price": 23200, "storage":"64GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-green-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"C005", "color": "blue",  "price": 23200, "storage":"64GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-blue-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"D001", "color": "gray",  "price": 28200, "storage":"256GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-spacegray-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650646000" },
+    { "id":"D002", "color": "silver",  "price": 28200, "storage":"256GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-silver-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650645000" },
+    { "id":"D003", "color": "rose",  "price": 28200, "storage":"256GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-gold-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"D004", "color": "green",  "price": 28200, "storage":"256GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-green-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" },
+    { "id":"D005", "color": "blue",  "price": 28200, "storage":"256GB", "network": "Cellular", "picture":"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-air-select-wifi-blue-202009?wid=940&hei=1112&fmt=png-alpha&.v=1598650644000" }
+]
+
+let watch = {
+    type: [
+        { size:"40", network:"GPS", price:14400 },
+        { size:"40", network:"Cellular", price:17400 },
+        { size:"44", network:"GPS", price:15400},
+        { size:"44", network:"Cellular", price:18400}
+    ],
+    picture: [
+        { size:"40",color:"rainbow", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MJX03ref_VW_34FR+watch-40-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW+watch-face-40-6s-pride_VW_34FR_WF_CO?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1619841295000,1601921879000,1620834614000"},
+        { size:"40",color:"black", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MY752ref_VW_34FR+watch-40-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1611115941000,1601921879000"},
+        { size:"40",color:"orange", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MJJ63ref_VW_34FR+watch-40-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1618338896000,1601921879000"},
+        { size:"44",color:"rainbow", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MJX93ref_VW_34FR+watch-44-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW+watch-face-44-6s-pride_VW_34FR_WF_CO?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1620367612000,1601922999000,1620841148000"},
+        { size:"44",color:"black", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MY8L2ref_VW_34FR+watch-44-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1611115834000,1601922999000"},
+        { size:"44",color:"orange", url:"https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MJJR3ref_VW_34FR+watch-44-alum-silver-cell-6s_VW_34FR_WF_CO_GEO_TW?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1617229195000,1601922999000"}
+    ]
+}
